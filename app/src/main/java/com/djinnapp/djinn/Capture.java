264ff94d8c86414a253bafc5fb8e83f0969f34e7
Ignacio Rasche <nacho.rasche@gmail.com>
2016-02-20 08:51:35 +0100

    Add ImageHD & ImageLD

--- app/src/main/java/com/djinnapp/djinn/Capture.java
+++ app/src/main/java/com/djinnapp/djinn/Capture.java
@@ -6,11 +6,16 @@ package com.djinnapp.djinn;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -22,46 +27,64 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
 import java.util.concurrent.Future;
 
 public class Capture extends Activity implements OnClickListener {
 
     ImageView mImageView;
-    Button cam;
+    ImageButton cam;
     String path;
+    Toast t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.capture_layout);
         mImageView = (ImageView) findViewById(R.id.img1);
-        cam = (Button) findViewById(R.id.camera);
+        cam = (ImageButton) findViewById(R.id.camera);
         cam.setOnClickListener(this);
-
     }
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_CAMERA = 137;
+    static final int CAPTURE_IMAGE_FULLSIZE = 12476;
 
-    private void dispatchTakePictureIntent() {
+    private void dispatchTakePictureIntent2() {
+        // Simple version of the take Picture intent
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
 
+    private void dispatchTakePictureIntent() {
+        // Simple version of the take Picture intent
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        File file = new File(Environment.getExternalStorageDirectory()+File.separator + "image.jpg");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+        startActivityForResult(intent, CAPTURE_IMAGE_FULLSIZE_ACTIVITY_REQUEST_CODE);
+
+    }
+
+    public static final int CAPTURE_IMAGE_FULLSIZE_ACTIVITY_REQUEST_CODE = 1777;
+
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
 
-
-            // TODO Save image file to path
-
+            Bitmap imageBitmap = (Bitmap) data.getExtras().get("data"); // Thumbnail
+            mImageView.setImageBitmap(imageBitmap);
 
             File f = new File(path);
             Future uploading = Ion.with(Capture.this)
-                .load("http://5c4ad84b.ngrok.com/upload")
+                .load("http://5c4ad84b.ngrok.com/upload-full")
                 .setMultipartFile("image", f)
                 .asString()
                 .withResponse()
@@ -79,8 +102,49 @@ public class Capture extends Activity implements OnClickListener {
                     }
                 });
 
-            mImageView.setImageBitmap(imageBitmap);
         }
+
+        if (requestCode == CAPTURE_IMAGE_FULLSIZE_ACTIVITY_REQUEST_CODE)
+        {
+            //Get our saved file into a bitmap object:
+            File file = new File(Environment.getExternalStorageDirectory()+File.separator + "image.jpg");
+
+            Bitmap imageHD = decodeSampledBitmapFromFile(file.getAbsolutePath(), 1000, 1000);
+            Bitmap imageLD = decodeSampledBitmapFromFile(file.getAbsolutePath(),100, 100);
+
+            mImageView.setImageBitmap(imageLD); // bitmap = HD
+
+
+        }
+    }
+
+    public static Bitmap decodeSampledBitmapFromFile(String path, int reqWidth, int reqHeight)
+    { // BEST QUALITY MATCH
+
+        //First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(path, options);
+
+        // Calculate inSampleSize, Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        options.inPreferredConfig = Bitmap.Config.RGB_565;
+        int inSampleSize = 1;
+        if (height > reqHeight)
+        {
+            inSampleSize = Math.round((float)height / (float)reqHeight);
+        }
+        int expectedWidth = width / inSampleSize;
+        if (expectedWidth > reqWidth)
+        {
+            //if(Math.round((float)width / (float)reqWidth) > inSampleSize) // If bigger SampSize..
+            inSampleSize = Math.round((float)width / (float)reqWidth);
+        }
+        options.inSampleSize = inSampleSize;
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFile(path, options);
     }
 
     @Override
