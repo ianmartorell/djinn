f3b587d839d15b9bbb7a6e0889998fb05941d662
Ian Martorell <ianmartorell@gmail.com>
2016-02-21 12:43:27 +0100

    Made GalleryActivity the main activity

--- app/src/main/java/com/djinnapp/djinn/GalleryActivity.java
+++ app/src/main/java/com/djinnapp/djinn/GalleryActivity.java
@@ -7,10 +7,13 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.gson.JsonArray;
@@ -23,7 +26,7 @@ import java.util.ArrayList;
 /**
  * Created by nacho on 20/02/2016.
  */
-public class GalleryActivity extends Activity {
+public class GalleryActivity extends AppCompatActivity {
     private static String LOG_TAG = "GalleryActivity";
     private RecyclerView galleryRecyclerView;
     private RecyclerView.Adapter thumbAdapter;
@@ -76,6 +79,29 @@ public class GalleryActivity extends Activity {
             }
         });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_gallery, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_joinevent) {
+            startActivity(new Intent("com.djinapp.djinn.CREATE"));
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     private void updateDataSet() {
 
         String eventId = getApplicationContext().getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
